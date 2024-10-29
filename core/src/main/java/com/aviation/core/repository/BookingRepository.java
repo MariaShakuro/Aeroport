966e1411826f39789e89a7a8f8e9db8ea3b1ec49
MariaShakuro <mashashakuro25@gmail.com>
2024-10-29 23:18:04 +0300

    create repositories

---
+++ core/src/main/java/com/aviation/core/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.aviation.core.repository;
+
+import com.aviation.core.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+}
---
+++ core/src/main/java/com/aviation/core/repository/FlightRepository.java
@@ -0,0 +1,10 @@
+package com.aviation.core.repository;
+
+import com.aviation.core.entity.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight,Long> {
+
+}
