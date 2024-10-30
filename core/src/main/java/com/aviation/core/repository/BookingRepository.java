e59b8fe188a6e43b5c6e4e5ece30f7398debc750
MariaShakuro <mashashakuro25@gmail.com>
2024-10-30 20:35:59 +0300

    rebuilding of project for create microservice architecture

--- core/src/main/java/com/aviation/core/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aviation.core.repository;
-
-import com.aviation.core.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking,Long> {
-}
